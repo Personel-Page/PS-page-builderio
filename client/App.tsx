b66f1249147e5f14bda1a63415bfa89d21f4d794
PhuocSang44 <phuocsang4405@gmail.com>
2025-09-20 15:53:07 +0700

    cai basename trong Routerrrr

--- client/App.tsx
+++ client/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/PS-page-builderio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
